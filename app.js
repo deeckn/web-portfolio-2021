e63e3c580e199266304504a21482e502f95f000a
deeckn <dee28511@gmail.com>
2022-01-08 14:08:23 +0700

    Reformat code and added comments

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const menuButton = document.querySelector("header nav img");
 menuButton.addEventListener('click', () => {
     navLinks.classList.toggle("navActive");
 });
+
+const repoButton = document.querySelector(".projectsSection a");
+// repoButton.href = "/#contact";
